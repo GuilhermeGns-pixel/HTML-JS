99ae28c27c999d48714ada723b78eb807aa3e715
Guilherme Nascimento <guisantos2005.2013@gmail.com>
2024-12-22 17:58:19 -0300

    implementacao

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ let campoA = document.getElementById('campoA');
 let campoB = document.getElementById('campoB');
 
 function campos (campoA, campoB){
-    const mensagemSucess = `O Campo B: <b>${campoB.value}</b> e maior do que o Campo A: <b>${campoA.value}</b> `
-    if (parseInt(campoB.value) > parseInt(campoA.value)){
+
+        if (parseInt(campoB.value) > parseInt(campoA.value)){
         document.querySelector('.sucess-mensagem').innerHTML = mensagemSucess;
         document.querySelector('.sucess-mensagem').style.display = 'block'
         campoA.value = '';
@@ -13,7 +13,7 @@ function campos (campoA, campoB){
         
     }
     else{
-        document.querySelector('.error-mensagem').innerHTML = mensagemSucess;
+        document.querySelector('.error-mensagem').innerHTML = mensagemError;
         document.querySelector('.error-mensagem').style.display = 'block'
         return campoA;
     }
@@ -23,10 +23,32 @@ function campos (campoA, campoB){
 form.addEventListener('submit', function (e){
     e.preventDefault();
    campos(campoA, campoB);
+    document.querySelector('.error-mensagem').style.display = 'none'
+    document.querySelector('.sucess-mensagem').style.display = 'none'
+        
 
-
+        //apagar itens q aparecem
 })
 
 
+campoB.addEventListener('keyup', function (e) {
+    console.log(e.target.value)
+    valida = (parseInt(campoB.value) > parseInt(campoA.value))
+
+    const mensagemSucess = `O Campo B : <b>${campoB.value}</b> e maior do que o Campo A: <b>${campoA.value}</b> `
+    const mensagemError = `O Campo A : <b>${campoA.value}</b> e maior do que o Campo B : <b>${campoB.value}</b> `
+
+    if (valida) {
+        document.querySelector('.sucess-mensagem').innerHTML = mensagemSucess;
+        document.querySelector('.sucess-mensagem').style.display = 'block'
+        document.querySelector('.error-mensagem').style.display = 'none'
+    }
+    else {
+        document.querySelector('.error-mensagem').innerHTML = mensagemError;
+        document.querySelector('.error-mensagem').style.display = 'block'
+        document.querySelector('.sucess-mensagem').style.display = 'none' 
+    }
+
+})
 console.log(form);
 
